9518e637dfa685437f244ef2f8599c79931bf5ab
Sallar Kaboli <sallar@kaboli.org>
2020-05-02 15:40:17 +0300

    Add search results

---
+++ src/api/books.ts
@@ -0,0 +1,27 @@
+export interface GoogleBook {
+  id: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    description: string;
+    imageLinks: {
+      smallThumbnail: string;
+      thumbnail: string;
+    };
+  };
+}
+
+export const searchBooks = async (query: string): Promise<GoogleBook[]> => {
+  try {
+    const results = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+        query,
+      )}`,
+    );
+    const json = await results.json();
+    return (json.items as GoogleBook[]).filter(book => book.volumeInfo.imageLinks?.thumbnail);
+  } catch (err) {
+    return [];
+  }
+};
--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,12 +1,79 @@
-import React from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, ScrollView, Image, StyleSheet} from 'react-native';
+import {useNavigationSearchBarUpdate} from 'react-native-navigation-hooks';
+import {searchBooks, GoogleBook} from '../api/books';
 
-const SearchScreen: React.FunctionComponent = () => {
+const SearchScreen: React.FunctionComponent<{componentId: string}> = ({
+  componentId,
+}) => {
+  const [query, setQuery] = useState<string>('skyward');
+  const [results, setResults] = useState<GoogleBook[]>([]);
+
+  useNavigationSearchBarUpdate(e => {
+    if (e.isFocused) {
+      setQuery(e.text);
+    } else {
+      setQuery('');
+    }
+  }, componentId);
+
+  useEffect(() => {
+    let timeout;
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+    if (query === '') {
+      return;
+    }
+    timeout = setTimeout(() => {
+      searchBooks(query).then(res => {
+        setResults(res);
+      });
+    }, 2000);
+  }, [query]);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: 'white'}}>Search Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.listContainer}>
+        {results.map(book => {
+          return (
+            <View key={book.id} style={styles.listItem}>
+              <Image
+                resizeMode="contain"
+                style={{width: 150, height: 200}}
+                source={{
+                  uri: book.volumeInfo.imageLinks.thumbnail.replace(
+                    'http://',
+                    'https://',
+                  ),
+                }}
+              />
+              <Text style={styles.listTitle}>{book.volumeInfo.title}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  listContainer: {
+    paddingVertical: 16,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  listItem: {
+    width: '50%',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  listTitle: {
+    marginTop: 8,
+    color: 'white',
+  },
+});
 
 export default SearchScreen;
